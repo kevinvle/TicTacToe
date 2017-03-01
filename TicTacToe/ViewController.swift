8399904faff40563df01cd17db44d80789a488aa
Kevin Viet Le <kevin1le@hotmail.com>
2017-03-01 04:16:01 -0800

    Work

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -11,14 +11,40 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    // 1 is noughts, 2 is player
+ 
+    var activePlayer = 1
+    
+    var gameState = [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0] // 0 - empty, 1 - nought, 2 - crosses
     
     
     @IBAction func buttonPressed(_ sender: AnyObject) { // Note I had to add "Object" after "Any". Reference: https://stackoverflow.com/questions/39606073/cant-drag-button-action-to-existing-function-in-xcode-8
         
+        let activePosition = sender.tag - 1 // We're redefining activePosition each time the buttonPressed method is called, we use let instead of var
         
+        if gameState[activePosition] == 0 {
+            
+            gameState[activePosition] = activePlayer
+            
+            
+        if activePlayer == 1 {
+            
+            sender.setImage(UIImage(named: "nought.png"), for: []) // Empty array to specify the default state
+            
+            activePlayer = 2
+            
+            
+            
+            
+        } else {
+            
+            sender.setImage(UIImage(named: "cross.png"), for: []) // Empty array to specify the default state
+            
+            activePlayer = 1
+            
+            }
         
-        sender.setImage(UIImage(named: "nought.png"), for: []) // Empty array to specify the default state
-        
+        }
         
         print(sender.tag)
         
