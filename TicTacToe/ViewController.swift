25f66accff68f485c4e38bb580b637c0c1d2c62e
Kevin Viet Le <kevin1le@hotmail.com>
2017-03-01 16:19:49 -0800

    Final

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -11,13 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var winnerLabel: UILabel!
+
+    @IBOutlet var playAgainButton: UIButton!
     
-    @IBOutlet var playAgainButton: UIButton! // Button label will change
-    
-    @IBAction func playAgain(_ sender: Any) { // Button Action
+    @IBAction func playAgain(_ sender: AnyObject) {
+
+         activeGame = true
         
-    }
+         activePlayer = 1
+        
+         gameState = [0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0] // 0 - empty, 1 - nought, 2 - crosses
+        
+        for i in 1..<10 { // 1 to 10 and 1 less than 10
+            
+            if let button = view.viewWithTag(i) as? UIButton { // You should set i to non zero. Cast that to UIButton because view is returning a general view which is any kind of UI element and we want to use some button specific features so we need to cast it as a button.
+                
+                button.setImage(nil, for: []) // Get rid of images for normal state
+            }
+            
+            winnerLabel.isHidden = true
+            playAgainButton.isHidden = true
+            
+            winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+            
+            playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+            
+        }
     
+    }
+
     // 1 is noughts, 2 is player
     
     var activeGame = true
@@ -58,6 +80,10 @@ class ViewController: UIViewController {
                     
                     activeGame = false
                     
+                    winnerLabel.isHidden = false
+                    
+                    playAgainButton.isHidden = false
+                    
                     if (gameState[combination[0]]) == 1 { // Number of the winner
                         
                         winnerLabel.text = "Nought has won!"
@@ -67,16 +93,13 @@ class ViewController: UIViewController {
                         winnerLabel.text = "Crosses has won!"
                     }
                     
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                    UIView.animate(withDuration: 1, animations: {
+                        
+                        self.winnerLabel.center = CGPoint(x: self.winnerLabel.center.x + 500, y: self.winnerLabel.center.y) // We're inside a closure happening outside the main thread. So we need to use self to make oit clear that we're referring to something outside of the closure and specifically in the view controller
+                        
+                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x + 500, y: self.playAgainButton.center.y)
+                        
+                    })
                     
                 }
             }
@@ -84,14 +107,6 @@ class ViewController: UIViewController {
         }
    
     }
-    
-    
-    
-    
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -101,14 +116,7 @@ class ViewController: UIViewController {
         playAgainButton.isHidden = true
         
         winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
-        
         playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
-        
-        
-        
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
